ce9c963a557473d0101ba28d90594f7f3a7d2304
AgusBirman <agusbirman@gmail.com>
2025-04-13 19:24:26 +0200

    changed logic to post profile picture

--- src/app/(api)/trainer/route.ts
+++ src/app/(api)/trainer/route.ts
@@ -2,13 +2,13 @@
  * @swagger
  * /trainer:
  *   post:
- *     summary: Registrar un nuevo entrenador
+ *     summary: Crear un nuevo entrenador con imagen de perfil opcional
  *     tags:
  *       - Trainer
  *     requestBody:
  *       required: true
  *       content:
- *         application/json:
+ *         multipart/form-data:
  *           schema:
  *             type: object
  *             properties:
@@ -24,6 +24,10 @@
  *                 type: string
  *               date_birth:
  *                 type: string
+ *                 format: date
+ *               file:
+ *                 type: string
+ *                 format: binary
  *             required:
  *               - name
  *               - email
@@ -33,20 +37,15 @@
  *               - date_birth
  *     responses:
  *       201:
- *         description: Entrenador creado exitosamente
+ *         description: Entrenador creado correctamente
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                 newTrainer:
- *                   $ref: '#/components/schemas/Trainer'
+ *               $ref: '#/components/schemas/Trainer'
  *       400:
- *         description: Error de validación o email ya registrado
+ *         description: Error en la validación o en la creación
  *       500:
- *         description: Error del servidor al registrar el entrenador
+ *         description: Error del servidor al conectar con la base de datos
  */
 
 /**
@@ -75,11 +74,20 @@ import bcrypt from "bcryptjs";
 import connect from "@/lib/db";
 import { MongooseError } from "mongoose";
 import validate from "@/lib/validate";
+import { uploadToCloudinary } from "@/lib/cloudinary/uploadToCloudinary";
 
 export async function POST(req: Request) {
     try {
         await connect()
-        const { name, email, password, repeat_password, phone, date_birth } = await req.json()
+        const data = await req.formData();
+
+        const name = data.get("name")?.toString() || "";
+        const email = data.get("email")?.toString() || "";
+        const password = data.get("password")?.toString() || "";
+        const repeat_password = data.get("repeat_password")?.toString() || "";
+        const phone = data.get("phone")?.toString() || "";
+        const date_birth = data.get("date_birth")?.toString() || "";
+        const file = data.get("file") as File | null;
 
         validate.isValidName(name)
         validate.isValidEmail(email)
@@ -91,13 +99,28 @@ export async function POST(req: Request) {
             return NextResponse.json({ message: "passwords dont match. It should be the same" }, { status: 400 })
         }
 
-        const user = await Trainer.findOne({ email })
-        if (user) {
+        const existingUser = await Trainer.findOne({ email })
+        if (existingUser) {
             return NextResponse.json({ message: "email already used" }, { status: 400 })
         }
 
         const hash_password = bcrypt.hashSync(password, 10)
-        const newTrainer = await Trainer.create({ name, email, password: hash_password, phone, date_birth })
+
+        let profile_picture_url = "";
+        let profile_picture_id = "";
+
+        if (file && file instanceof File) {
+            const result = await uploadToCloudinary(file, {
+                folder: "TrainerProfilePicture",
+            });
+
+            profile_picture_url = result.secure_url;
+            profile_picture_id = result.public_id;
+        }
+
+        const newTrainer = await Trainer.create({
+            name, email, password: hash_password, phone, date_birth, profile_picture_url, profile_picture_id
+        })
 
         return NextResponse.json({ message: "Trainer had been created", newTrainer, status: 201 })
     } catch (error: any) {
