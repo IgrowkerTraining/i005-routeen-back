ce6a134143a225424f4e767f057145277dcf5440
Alejandro Viesca <109549664+ViesK@users.noreply.github.com>
2025-04-14 21:15:56 +0000

    push

--- src/models/Routine.ts
+++ src/models/Routine.ts
@@ -6,7 +6,7 @@ const RoutineSchema = new Schema(
     {
         name: { type: "String", required: true },
         description: { type: "String"},
-        trainer_id: { type: ObjectId, ref: Trainer, required: true },
+        trainer_id: { type: ObjectId, ref: "Trainer", required: true },
     },
     {
         timestamps: true
