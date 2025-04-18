9d20203ab83f84d5ab180ff83ae4589b7a998aaa
Alejandro Viesca <109549664+ViesK@users.noreply.github.com>
2025-04-18 00:30:35 +0000

    feat: swagger

--- src/app/(api)/athlete/name/route.ts
+++ src/app/(api)/athlete/name/route.ts
@@ -1,3 +1,50 @@
+/**
+ * @swagger
+ * /athlete:
+ *   get:
+ *     summary: Buscar atletas por nombre
+ *     tags:
+ *       - Athlete
+ *     parameters:
+ *       - name: name
+ *         in: query
+ *         description: Nombre del atleta para realizar la búsqueda.
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: "John Doe"
+ *     responses:
+ *       200:
+ *         description: Atletas encontrados con el nombre proporcionado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 athletes:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: "60d62b1f65b8b9f1f6b7d1c8"
+ *                       name:
+ *                         type: string
+ *                         example: "John Doe"
+ *                       email:
+ *                         type: string
+ *                         example: "john.doe@example.com"
+ *       400:
+ *         description: El nombre del atleta es obligatorio.
+ *       403:
+ *         description: El usuario debe ser un entrenador para realizar esta búsqueda.
+ *       404:
+ *         description: No se encontraron atletas con el nombre proporcionado.
+ *       500:
+ *         description: Error en la base de datos o en la búsqueda de los atletas.
+ */
+
 import { NextResponse } from "next/server";
 import Athlete from "@/models/Athlete";
 import connect from "@/lib/db";
