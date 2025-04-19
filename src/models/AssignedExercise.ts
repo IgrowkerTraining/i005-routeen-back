b44c9e438989c41e5e55c0244f4108279089f91d
Paula Wilshaw Muñoz <p.wilmun@gmail.com>
2025-04-19 20:37:04 +0200

    modified RoutineExercise and AssignedExercise models

--- src/models/AssignedExercise.ts
+++ src/models/AssignedExercise.ts
@@ -3,15 +3,24 @@ import { models, model, Schema } from "mongoose";
 import RoutineAssigned from "./RoutineAssigned";
 import Exercise from "./Exercise";
 
-const AssignedExerciseSchema = new Schema({
-  queue: { type: Number, required: true },
-  reps: { type: Number },
-  sets: { type: Number },
-  routine_weight: { type: Number },
-  rest_time: { type: Number },
-  assigned_routine_id: { type: ObjectId, ref: RoutineAssigned, required: true },
-  exercise_id: { type: ObjectId, ref: Exercise, required: true },
-});
+const AssignedExerciseSchema = new Schema(
+  {
+    queue: { type: Number, required: true },
+    reps: { type: Number },
+    sets: { type: Number },
+    routine_weight: { type: Number },
+    rest_time: { type: Number },
+    assigned_routine_id: {
+      type: ObjectId,
+      ref: RoutineAssigned,
+      required: true,
+    },
+    exercise_id: { type: ObjectId, ref: Exercise, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const AssignedExercise =
   models.AssignedExercise || model("AssignedExercise", AssignedExerciseSchema);
