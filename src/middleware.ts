b80ad7a82cf407763de5c2685c0a523fd9acda2b
AgusBirman <agusbirman@gmail.com>
2025-04-22 00:36:32 +0200

    allowed patch methods in cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
     response.headers.set('Access-Control-Allow-Origin', origin);
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     response.headers.set('Access-Control-Allow-Credentials', 'true');
 
